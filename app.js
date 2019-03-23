e97be3d516e8436b11c437d662b46e46b344429c
shirantha97 <herathshirantha@gmail.com>
2019-03-23 12:46:21 +0530

    synchronous file reading

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const fs = require('fs');
 
+// fs.writeFile('./test.txt',"Hello",(err, data) => {
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log('File was saved');
+// });
 const fileName = __dirname + '/test.txt';
-const outFileName = __dirname + '/test-copy.txt';
+//const outFileName = __dirname + '/test-copy.txt';
 
-const readStream = fs.createReadStream(fileName);
-const writeStream = fs.createWriteStream(outFileName);
+//const readStream = fs.createReadStream(fileName);
+//const writeStream = fs.createWriteStream(outFileName);
+
+//readStream.on('data', data => {
+//    console.log(data.toString());
+//})
 
 
-//Listening and writng
-readStream.on('data', data => {
-    console.log(data.toString());
-})
-
-//Copying
-readStream.pipe(writeStream);
+// readStream.pipe(writeStream);
 
 
 // fs.readFile(fileName, (err, data) => {
@@ -25,5 +30,16 @@ readStream.pipe(writeStream);
 // })
 
 
+//synchronous reading
+ const data = fs.readFileSync(fileName);
+ console.log(data.toString());
+
+
+//Os related stuff
+// const os = require('os');
+//
+// console.log('Architecture ' + os.arch());
+// console.log('CPUs ' + os.cpus().length);
+// console.log('OS ' + os.platform());
 
 
