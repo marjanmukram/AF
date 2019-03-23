fefaad8c895bb54c50e89d522206cded5fe43f6f
shirantha97 <herathshirantha@gmail.com>
2019-03-23 12:43:43 +0530

    Listening to the data event and write and normal writing

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const fs = require('fs');
 
-fs.writeFile('./test.txt',"Hello",(err, data) => {
-    if(err){
-        console.log(err);
-        return;
-    }
-    console.log('File was saved');
-});
+const fileName = __dirname + '/test.txt';
+const outFileName = __dirname + '/test-copy.txt';
+
+const readStream = fs.createReadStream(fileName);
+const writeStream = fs.createWriteStream(outFileName);
+
+
+//Listening and writng
+readStream.on('data', data => {
+    console.log(data.toString());
+})
+
+//Copying
+readStream.pipe(writeStream);
+
+
+// fs.readFile(fileName, (err, data) => {
+//     if(err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log(data);
+// })
+
+
 
 
