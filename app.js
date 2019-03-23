b363ac9b049dd9a269c78a72b5c12870becb1a8e
shirantha97 <herathshirantha@gmail.com>
2019-03-23 12:34:14 +0530

    Writeing to a file

---
+++ app.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+// fs.writeFile('./test.txt',"Hello",(err, data) => {
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log('File was saved');
+// });
+const fileName = __dirname + '/test.txt';
+const outFileName = __dirname + '/test-copy.txt';
+
+const readStream = fs.createReadStream(fileName);
+const writeStream = fs.createWriteStream(outFileName);
+
+readStream.on('data', data => {
+    console.log(data.toString());
+})
+
+
+// readStream.pipe(writeStream);
+
+
+// fs.readFile(fileName, (err, data) => {
+//     if(err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log(data);
+// })
+
+// const data = fs.readFileSync(fileName);
+// console.log(data.toString());
+
+
+
+// const os = require('os');
+//
+// console.log('Architecture ' + os.arch());
+// console.log('CPUs ' + os.cpus().length);
+// console.log('OS ' + os.platform());
+
+
